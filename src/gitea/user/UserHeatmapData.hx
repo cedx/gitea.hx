d49e283ac3f8110391a4dc6ed0e8f9d5a2902bca
Cédric Belin <cedric@belin.io>
2020-10-05 10:19:51 +0200

    Added the `UserHeatmapData` model

---
+++ src/gitea/user/UserHeatmapData.hx
@@ -0,0 +1,25 @@
+package gitea.user;
+
+import haxe.DynamicAccess;
+
+/** Represents the data needed to create a user heatmap. **/
+class UserHeatmapData {
+
+	/** The contribution number. **/
+	public var contributions = 0;
+
+	/** The contributions date.  **/
+	public var timestamp: Null<Date> = null;
+
+	/** Creates a new heatmap data. **/
+	public function new(contributions: Int, timestamp: Date) {
+		this.contributions = contributions;
+		this.timestamp = timestamp;
+	}
+
+	/** Creates a new heatmap data from the specified JSON map. **/
+	public static function fromJson(map: DynamicAccess<Int>) return new UserHeatmapData(
+		map.exists("contributions") ? map["contributions"] : 0,
+		map.exists("timestamp") ? Date.fromTime(map["timestamp"] * 1000) : null
+	);
+}
