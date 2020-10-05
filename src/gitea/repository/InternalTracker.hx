0ae956147801368f419127adec277dbe6661374f
Cédric Belin <cedric@belin.io>
2020-10-05 18:38:06 +0200

    Added the `InternalTracker` model

---
+++ src/gitea/repository/InternalTracker.hx
@@ -0,0 +1,30 @@
+package gitea.repository;
+
+import haxe.DynamicAccess;
+
+/** Represents settings for an internal tracker. **/
+@:expose
+@:structInit
+class InternalTracker {
+
+	/** Value indicating whether to allow only contributors to track time. **/
+	public var allowOnlyContributorsToTrackTime = false;
+
+	/** Value indicating whether to enable issue dependencies. **/
+	public var enableIssueDependencies = false;
+
+	/** Value indicating whether to enable the time tracker. **/
+	public var enableTimeTracker = false;
+
+	/** Creates a new internal tracker. **/
+	public function new () {}
+
+	/** Creates a new internal tracker from the specified JSON map. **/
+	public static function fromJson(map: DynamicAccess<Bool>) {
+		final model = new InternalTracker();
+		if (map.exists("allow_only_contributors_to_track_time")) model.allowOnlyContributorsToTrackTime = map["allow_only_contributors_to_track_time"];
+		if (map.exists("enable_issue_dependencies")) model.enableIssueDependencies = map["enable_issue_dependencies"];
+		if (map.exists("enable_time_tracker")) model.enableTimeTracker = map["enable_time_tracker"];
+		return model;
+	}
+}
