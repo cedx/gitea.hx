de901060a121b8076c7d3d17f1835ca3789e1818
Cédric Belin <cedric@belin.io>
2020-12-10 09:45:12 +0100

    Updated the build system

--- tool/dist.ps1
+++ tool/dist.ps1
@@ -7,7 +7,7 @@ tool/version.ps1
 tool/build.ps1
 
 if (-not (Test-Path build)) { New-Item build -ItemType Directory | Out-Null }
-Copy-Item lib/js/gitea.js build/gitea.js
+Copy-Item lib/js/bundle.js build/gitea.js
 npm run dist
 
 Get-ChildItem lib/php -Exclude gitea | Remove-Item -Force -Recurse
